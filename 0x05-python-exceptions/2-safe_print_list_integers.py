7d0c65fa01f990b5b76fa67312752f92fc48ac08
bigtammy1 <bigtammy1@gmail.com>
2022-12-19 21:47:15 +0000

    Safe print integer

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    num_print = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (TypeError, ValueError):
+            i += 1
+        else:
+            num_print += 1
+            i += 1
+    print()
+    return num_print
