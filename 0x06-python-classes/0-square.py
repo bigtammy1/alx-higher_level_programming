1215491584b0ce40c09b896b9c682c0783d60851
bigtammy1 <bigtammy1@gmail.com>
2022-12-20 16:58:31 +0000

    My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module contains the ``Square`` class (An empty class).
+"""
+
+
+class Square:
+    """An empty class which does nothing.
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+# README file for my ALX Project "0x06. Python - Classes and Objects"
+
