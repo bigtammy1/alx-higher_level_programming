e89d56ae895c83e2ad2f0685d6232be8ea64a844
bigtammy1 <bigtammy1@gmail.com>
2023-03-14 18:25:24 +0000

    script that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w !== 0 && w > 0) && (h !== 0 && h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let symbol = 'X';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 1; j < this.width; j++) {
+        symbol = symbol.concat('X');
+      }
+      console.log(symbol);
+      symbol = 'X';
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+};
