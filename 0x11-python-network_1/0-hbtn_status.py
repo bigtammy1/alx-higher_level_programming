4f6be67ac6045857d77fbe37f5709b54a7dc0781
bigtammy1 <bigtammy1@gmail.com>
2023-03-31 15:12:35 -0700

    done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""
-Prints different results of
-a request
-"""
+"""a python  script that fetches https://alx-intranet.hbtn.io/status
+using urllib and with statement"""
 import urllib.request
 
-
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
-        print("-\t type:", type(html))
-        print("-\t content:", html)
-        print("-\t utf8 content:", html.decode("utf-8"))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
